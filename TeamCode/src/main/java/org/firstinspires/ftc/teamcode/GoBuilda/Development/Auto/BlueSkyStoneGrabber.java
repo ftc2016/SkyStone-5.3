311c95630e8a900f543ceab00c34adb2be7bdcb7
ftc2016 <ftcteam11142@gmail.com>
2019-12-14 00:22:03 -0600

    14th - Before Scrim 4
    Garbage code is ready to be tested agaist slighlty etter garbage code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBuilda/Development/Auto/BlueSkyStoneGrabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBuilda/Development/Auto/BlueSkyStoneGrabber.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.GoBuilda.Development.Auto;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -21,6 +22,7 @@ import static java.lang.Math.abs;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
+@Disabled
 @Autonomous(name="Blue SkyStoneGrabbber", group = "Blue")
 public class BlueSkyStoneGrabber extends LinearOpMode {
 
